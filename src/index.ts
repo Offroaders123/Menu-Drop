75462af5b14729135241c9fd3a2f7a9f9d4e102e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-04-05 13:09:01 -0700

    Appendable Adopted Style Sheets
    Now you can push to adopted stylesheets arrays. Awesome! This appears to be in Baseline for all browsers too.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ export * from "./MenuSubList.js";
 import styles from "../styles/style.css?inline" with { type: "css" };
 
 const stylesheet: CSSStyleSheet = typeof styles === "string"
-  ? await new CSSStyleSheet().replace(styles)
+  ? ((): CSSStyleSheet => {
+    const stylesheet: CSSStyleSheet = new CSSStyleSheet();
+    stylesheet.replaceSync(styles);
+    return stylesheet;
+  })()
   : styles;
 
-document.adoptedStyleSheets = [...document.adoptedStyleSheets,stylesheet];
+(document.adoptedStyleSheets as CSSStyleSheet[]).push(stylesheet);
